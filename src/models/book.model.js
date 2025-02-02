a2653fa1baa2d5fc23905350b2571e8a931d4007
jptlearn <jptcode500@gmail.com>
2025-02-02 14:25:31 +0545

    create bookmodel

---
+++ src/models/book.model.js
@@ -0,0 +1,30 @@
+import sequelize from "./index.js";
+import { DataTypes } from "sequelize";
+
+const bookModel = sequelize.define("books", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  genre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  image: {
+    type: DataTypes.STRING,
+  }
+})
+
+export default bookModel;
