3aa3ad6477a373c6617bb107afaaec49c91954d5
jptlearn <jptcode500@gmail.com>
2025-02-04 15:31:42 +0545

    update backend code

--- backend/src/controllers/book.controller.js
+++ backend/src/controllers/book.controller.js
@@ -10,27 +10,27 @@ const addBook = async (req, res) => {
       author,
       genre,
       description,
-      image: image
+      image: image,
     });
     res.json({ success: true, message: "Book added successfully" });
   } catch (error) {
     console.error("Error while adding book", error);
   }
-}
+};
 
 const getBookByID = async (req, res) => {
   const { id } = req.params;
   try {
     if (id) {
-      const book = await bookModel.findByPk(id)
-      res.json({ success: true, message: book })
+      const book = await bookModel.findByPk(id);
+      res.json({ success: true, message: book });
     } else {
-      res.json({ success: false, message: "Id is required." })
+      res.json({ success: false, message: "Id is required." });
     }
   } catch (error) {
     console.log("error while fetching book: ", error);
   }
-}
+};
 
 const updateBook = async (req, res) => {
   const { id } = req.params;
@@ -38,73 +38,85 @@ const updateBook = async (req, res) => {
   if (id) {
     const book = await bookModel.findByPk(id);
     if (!book) {
-      return res.json({ message: "Book not found with this id." })
+      return res.json({ message: "Book not found with this id." });
     }
     const { name, author, description, genre } = req.body;
-    await bookModel.update({ name, author, description, genre }, {
-      where: {
-        id
+    await bookModel.update(
+      { name, author, description, genre },
+      {
+        where: {
+          id,
+        },
       }
-    })
-    res.json({ success: true, message: "book updated successfully!!" })
+    );
+    res.json({ success: true, message: "book updated successfully!!" });
   } else {
-    res.json({ success: false, message: "Book Id not provided." })
+    res.json({ success: false, message: "Book Id not provided." });
   }
-}
+};
 
 const deleteBook = async (req, res) => {
   const { id } = req.params;
 
   if (!id) {
-    return res.json({ message: "Book Id not provided." })
+    return res.json({ message: "Book Id not provided." });
   }
   const data = await bookModel.destroy({
     where: {
       id,
-    }
+    },
   });
   if (data) {
-    res.json({ success: true, message: "Book deleted." })
+    res.json({ success: true, message: "Book deleted." });
   } else {
     res.json({
-      success: false, message: "Error deleting book"
-    })
+      success: false,
+      message: "Error deleting book",
+    });
   }
-}
+};
 
 const searchBook = async (req, res) => {
   const { q } = req.query;
   const { or } = Op; // Destructure Op for cleaner code
   try {
     if (!q) {
-      return res.json({ success: false, message: "No query was found in the string." });
+      return res.json({
+        success: false,
+        message: "No query was found in the string.",
+      });
     }
     const data = await bookModel.findAll({
       where: {
         [or]: [
           { name: { [Op.like]: `%${q}%` } },
-          { author: { [Op.like]: `%${q}%` } }
-        ]
-      }
+          { author: { [Op.like]: `%${q}%` } },
+        ],
+      },
     });
     console.log(data);
     res.json(data);
   } catch (error) {
-    res.send("Error while searching book")
+    res.send("Error while searching book");
   }
-}
+};
 
 const getBooks = async (req, res) => {
   let { limit } = req.query;
-  limit = limit ? parseInt(limit, 10) : 20; //
+  if (!limit) limit = 20;
   try {
-    const books = await bookModel.findAll({
-      limit: limit,
+    const data = await bookModel.findAll({
+      limit,
     });
-    res.json({ success: true, message: "All books fetched successfully.", data: books })
+    console.log(data);
+    for (let d of data) {
+      // console.log(d.dataValues);
+      d.dataValues.image = "http://localhost:8000/" + d.dataValues.image;
+    }
+    res.json(data);
   } catch (error) {
-    res.json({ success: false, message: "Error while fetching books." })
+    res.send("Error while fetching data");
   }
-}
+};
 
-export { addBook, getBookByID, updateBook, deleteBook, searchBook, getBooks }
+export { addBook, getBookByID, updateBook, deleteBook, searchBook, getBooks };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,28 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import sequelize from "./models/index.js";
-import bookRoutes from "./routes/book.route.js"
+import bookRoutes from "./routes/book.route.js";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-  res.send("Backend is working")
-})
+  res.send("Backend is working");
+});
 
-app.use(express.static("./public/uploads/"))
+app.use(express.static("./public/uploads/"));
 app.use("/book", bookRoutes);
 
 app.listen(process.env.PORT, async () => {
-  console.log("Server is running in port 8000...🚀")
+  console.log("Server is running in port 8000...🚀");
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ force: false, alter: true })
+    await sequelize.sync({ force: false, alter: true });
     console.log("Database connected successfully!!");
   } catch (error) {
-    console.error("Error during database connection.", error)
+    console.error("Error during database connection.", error);
   }
-})
+});
