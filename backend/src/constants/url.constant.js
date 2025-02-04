e083e003a7a3fb98b00aedb92c376ce452f84a18
jptlearn <jptcode500@gmail.com>
2025-02-04 23:09:39 +0545

    changes in code

--- backend/src/constants/url.constant.js
+++ backend/src/constants/url.constant.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:8000";
+
+const API_URL = process.env.BACKEND_URL + "http://localhost:8000"
 
 export default {
   IMG_PATH_URL: `${API_URL}/`,
--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import "dotenv/config";
 
 const baseURL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
