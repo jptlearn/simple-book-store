389e442ed1ed25342e15228379f0a272573cb816
jptlearn <jptcode500@gmail.com>
2025-02-04 22:49:00 +0545

    refactor and clean existing code

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   dialect: "mysql",
   pool: {
     max: 10,
