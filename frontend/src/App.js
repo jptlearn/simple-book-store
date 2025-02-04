706280106ffd268a15d2a974c17e324ae5c58426
jptlearn <jptcode500@gmail.com>
2025-02-04 20:11:19 +0545

    add listbook route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import AddBook from "./pages/AddBook";
 import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
 import "./assets/sass/main.scss";
+import Explore from "./pages/Explore";
+import ListBook from "./pages/ListBook";
 
 function App() {
   return (
@@ -11,8 +13,25 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard">
           <Route index element={<Dashboard />} />
-          <Route path="addbook" element={<AddBook />} />
+          <Route path="addBook" element={<AddBook />} />
+          <Route path="listBook" element={<ListBook />} />
         </Route>
+        <Route
+          path="*"
+          element={
+            <div
+              style={{
+                color: "black",
+                textAlign: "center",
+                marginTop: "50px",
+                fontSize: "24px",
+              }}
+            >
+              (404)Page not found
+            </div>
+          }
+        />
+        <Route path="/explore" element={<Explore />} />
       </Routes>
     </Router>
   );
